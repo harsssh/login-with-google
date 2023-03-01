e199f364183f90fc4e5dcd0880267c9dea885de1
harsssh <66548698+harsssh@users.noreply.github.com>
2023-03-01 21:27:02 +0900

    env

--- auth/google.ts
+++ auth/google.ts
@@ -1,10 +1,10 @@
 import { google } from 'googleapis'
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, HOST } = process.env
 const oauth2Client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-  'http://localhost:3000/api/callback'
+  `http://${HOST}/api/callback`
 )
 
 export { oauth2Client }
